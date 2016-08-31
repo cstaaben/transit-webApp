4285198aca4a7e2bac05ee3f4976dfbab168a8ca
Timothy Baumgartner <timmey.g.b@gmail.com>
2016-08-31 13:29:43 -0700

    Refactored code for getting trips.

--- js/busForm.js
+++ js/busForm.js
@@ -1,258 +1,250 @@
-$( document ).ready(function() {
-	//PUT THESE IN CSS
+$(document).ready(function () {
 
+    //initForm();
+    $("#map").slideUp();
+    $(".menu .item").tab();
+    $("p").css("padding", "10px");
+    $("#routeMap").slideUp();
+    $("#stops").slideUp();
+    $("h3").hide();
 
-	//initForm();
-	$("#map").slideUp();
-	$(".menu .item").tab();
-	$("p").css("padding","10px");
-	$("#routeMap").slideUp();
-	$("#stops").slideUp();
-	$("h3").hide();
-
-	$(".invalid").hide();
-	$(".formBody").hide();
-	$("#findStops").show();
-	$("#viewSchedule").hide();
-	$("#dateForm").hide();
-	$("#warning").hide();
-	//initMap();
-	getRoutes();
-	initForm();
-	setMenu();
+    $(".invalid").hide();
+    $(".formBody").hide();
+    $("#findStops").show();
+    $("#viewSchedule").hide();
+    $("#dateForm").hide();
+    $("#warning").hide();
+    //initMap();
+    getRoutes();
+    initForm();
+    setMenu();
 
     //console.log( "ready!" );
 
-	$("#btnSubmit").click(submitClick);
-	$("#btnTripSubmit").click(tripSubmit);
-	$("#btnRouteSubmit").click(getRoute);
+    $("#btnSubmit").click(submitClick);
+    $("#btnTripSubmit").click(tripSubmit);
+    $("#btnRouteSubmit").click(getRoute);
 
-	$("#fullRouteAddFave").click(function(){
-		var fArray = getFavorites();
-		var routeName = "Route: ";
-		routeName += $("#allRoutes option:selected").text();
-		var routeId = $("#allRoutes").val();
-		
-		if(fArray === undefined){//favorites is empty
-			addToFaves(routeName, routeId);
-		}else{
-			if(lookForFave(routeName)){//route exists in favorites already
-				alert(routeName +" is already in your favorites!");
-			}else{
-				addToFaves(routeName,routeId);
-			}
-		}
-	});
-	
+    $("#fullRouteAddFave").click(function () {
+        var fArray = getFavorites();
+        var routeName = "Route: ";
+        routeName += $("#allRoutes option:selected").text();
+        var routeId = $("#allRoutes").val();
+
+        if (fArray === undefined) {//favorites is empty
+            addToFaves(routeName, routeId);
+        } else {
+            if (lookForFave(routeName)) {//route exists in favorites already
+                alert(routeName + " is already in your favorites!");
+            } else {
+                addToFaves(routeName, routeId);
+            }
+        }
+    });
+
 });
 
 function getDate() {
-//GET CURRENT DATE/TIME
-	var date = new Date($.now());
+    //GET CURRENT DATE/TIME
+    var date = new Date($.now());
+
+    //SET TIME AND DATE DATA TO WORK FOR INPUT FIELDS
+    var month = date.getMonth() + 1;
+    if (month < 10)
+        month = "0" + month;
+
+    var day = date.getDate();
+    if (day < 10)
+        day = "0" + day;
+
+    //SET TIME AND DATE TO CORRECT FORMAT
+    return date.getFullYear() + "-" + month + "-" + day;
+
+}
+
+function getTime() {
+    var date = new Date($.now());
+    var hour = date.getHours();
+    if (hour < 10)
+        hour = "0" + hour;
+
+    var minutes = date.getMinutes();
+    if (minutes < 10)
+        minutes = "0" + minutes;
+
+    return hour + ":" + minutes;
+}
+
+function initForm() {
+    //POPULATE FORM
+    var currentTime = getTime;
+    var currentDate = getDate;
+
+    $("#time").val(currentTime);
+    $("#date").val(currentDate)
+}
+
+function submitClick() {
+    if ($("#location").val() == "") {
+        console.log("empty Location");
+        stopsValidation();
+    }
+
+    else {
+
+        $(".invalid").hide();
+        $("#stops").slideDown(500);
+        //clearMarkers();
+        // automatically scroll down to stops div
+        $('html,body').animate({
+                scrollTop: $("#stops").offset().top
+            },
+            'slow');
+
+        var date = new Date($.now());
+        var time = $("#time").val() + ":" + date.getSeconds();
+        var location = $("#location").val();
+        var submitDate = $("#date").val();
+        //debugger;
+        // console.log(location);
+        //  console.log(time);
+        //  console.log(submitDate);
+
+        getGeoCoding(location, submitDate, time);
+    }//end else
+}
+
+function tripSubmit() {
+    $(".invalid").hide();
+
+    if ($("#starting").val() == "" || $("#destination").val() == "") {
+        routeValidation();
+        return;
+    }
+
+    $("#routes").empty();
+    var origin = $("#starting").val();
+    var destination = $("#destination").val();
+    var date = $("#date2").val();
+    var time = $("#time2").val();
+    var sortBy = $("#timeType").val() === "arriveBy";
+    getTrips(origin, destination, date, time, sortBy);
+}
+
+
+function setMenu() {
+    $(".findStopsMenu").click(function () {
+        $("li").removeClass("active");
+        $(".formBody").hide();
+        $("#findStops").show();
+        $(".findStopsMenu").addClass("active");
+        $(".invalid").hide();
+        $("#map").hide();
+        $("#routeMap").hide();
+
+    });
+
+    $(".planTripMenu").click(function () {
+        $("li").removeClass("active");
+        $(".formBody").hide();
+        $("#planTrip").show();
+        populateRouteForm();
+        $(".planTripMenu").addClass("active");
+        $(".invalid").hide();
+        $("#map").hide();
+        $("#routeMap").hide();
+        $("#stops").hide();
+
+    });
+
+    $(".favoritesMenu").click(function () {
+        $("li").removeClass("active");
+        $(".formbody").hide();
+        $("#favorites").show();
+        $(".favoritesMenu").addClass("active");
+        $(".invalid").hide();
+        $("#map").hide();
+        $("#routeMap").hide();
+        $("#stops").hide();
+
+    });
+
+    $(".getRouteMenu").click(function () {
+        $("li").removeClass("active");
+        $(".formbody").hide();
+        $("#getRoute").show();
+        $(".getRouteMenu").addClass("active");
+        $(".invalid").hide();
+        $("#map").hide();
+        $("#routeMap").hide();
+        $("#stops").hide();
+        populateRouteDate();
+    });
+}
+
+function populateRouteDate() {
+    var date = getDate;
+    $("#routeDate").val(date);
+}
+
+function viewSchedule() {
+    var date = $("#routeDate").val();
+    var route = $("#allRoutes").val();
+
+    getAllStops(route, date);
+}
+
+function populateRouteForm() {
+    console.log("Route Clicked");
+    var date = new Date($.now());
 
 //SET TIME AND DATE DATA TO WORK FOR INPUT FIELDS	
-	var month = date.getMonth()+1;
-	if(month < 10)
-		month = "0"+month;
-	
-	var day = date.getDate();
-	if(day < 10)
-		day = "0"+day;
+    var month = date.getMonth() + 1;
+    if (month < 10)
+        month = "0" + month;
 
-//SET TIME AND DATE TO CORRECT FORMAT
-	
-	return date.getFullYear()+"-"+month+"-"+day;
-	
-}
+    var day = date.getDate();
+    if (day < 10)
+        day = "0" + day;
 
-function getTime(){
-	var date = new Date($.now());
-		var hour = date.getHours();
-	if(hour < 10)
-		hour = "0"+hour;
+    var hour = date.getHours();
+    if (hour < 10)
+        hour = "0" + hour;
 
-	var minutes = date.getMinutes();
-	if(minutes < 10)
-		minutes = "0"+minutes;
-
-	return hour+":"+minutes;
-}
-
-function initForm(){
-	//POPULATE FORM
-	var currentTime = getTime;
-	var currentDate = getDate;
-
-	$("#time").val(currentTime);
-	$("#date").val(currentDate)
-}
-
-function submitClick()
-{
-	if($("#location").val()=="")
-	{
-		console.log("empty Location");
-		stopsValidation();
-	}
-
-	else{
-		
-		$(".invalid").hide();
-		$("#stops").slideDown(500);
-		//clearMarkers();
-		// automatically scroll down to stops div
-		$('html,body').animate({
-			   scrollTop: $("#stops").offset().top},
-			   'slow');
-		
-		var date = new Date($.now());
-		var time = $("#time").val()+":"+date.getSeconds();
-		var location = $("#location").val();
-		var submitDate = $("#date").val();
-		//debugger;
-		// console.log(location);
-		//  console.log(time);
-		//  console.log(submitDate);
-
-		getGeoCoding(location, submitDate, time);
-	}//end else	
-}
-
-function tripSubmit(){
-	$(".invalid").hide();
-
-	if( $("#starting").val()=="" || $("#destination").val() == ""){
-		routeValidation();
-	}
-
-	else{
-		if($("#timeType").val() === "arriveBy"){
-			getTrips_arriveBy($("#starting").val(), $("#destination").val(), $("#date2").val(), $("#time2").val());
-		}else{
-			console.log("departing");
-			getTrips_departAt($("#starting").val(), $("#destination").val(), $("#date2").val(), $("#time2").val());
-			$("#routes").empty();
-		}
-		$(".invalid").hide();
-	}
-
-}
-
-
-function setMenu(){
-	$(".findStopsMenu").click(function(){
-		$("li").removeClass("active");
-		$(".formBody").hide();
-		$("#findStops").show();
-		$(".findStopsMenu").addClass("active");
-		$(".invalid").hide();
-		$("#map").hide();
-		$("#routeMap").hide();
-		
-	});
-
-	$(".planTripMenu").click(function(){
-		$("li").removeClass("active");
-		$(".formBody").hide();
-		$("#planTrip").show();
-		populateRouteForm();
-		$(".planTripMenu").addClass("active");
-		$(".invalid").hide();
-		$("#map").hide();
-		$("#routeMap").hide();
-		$("#stops").hide();
-		
-	});
-	
-	$(".favoritesMenu").click(function(){
-		$("li").removeClass("active");
-		$(".formbody").hide();
-		$("#favorites").show();
-		$(".favoritesMenu").addClass("active");
-		$(".invalid").hide();
-		$("#map").hide();
-		$("#routeMap").hide();
-		$("#stops").hide();
-		
-	});
-
-	$(".getRouteMenu").click(function(){
-		$("li").removeClass("active");
-		$(".formbody").hide();
-		$("#getRoute").show();
-		$(".getRouteMenu").addClass("active");
-		$(".invalid").hide();
-		$("#map").hide();
-		$("#routeMap").hide();
-		$("#stops").hide();
-		populateRouteDate();
-	});
-}
-
-function populateRouteDate(){
- 	var date = getDate;
- 	$("#routeDate").val(date);
- }
-
-function viewSchedule(){
-	var date = $("#routeDate").val();
-	var route = $("#allRoutes").val();
- 	
-	getAllStops(route, date);
-}
-
-function populateRouteForm(){
-	console.log("Route Clicked");
-	var date = new Date($.now());
-
-//SET TIME AND DATE DATA TO WORK FOR INPUT FIELDS	
-	var month = date.getMonth()+1;
-	if(month < 10)
-		month = "0"+month;
-
-	var day = date.getDate();
-	if(day < 10)
-		day = "0"+day;
-
-	var hour = date.getHours();
-	if(hour < 10)
-		hour = "0"+hour;
-
-	var minutes = date.getMinutes();
-	if(minutes < 10)
-		minutes = "0"+minutes;
+    var minutes = date.getMinutes();
+    if (minutes < 10)
+        minutes = "0" + minutes;
 
 //SET TIME AND DATE TO CORRECT FORMAT
 
-	var currentTime = hour+":"+minutes;
-	var currentDate = date.getFullYear()+"-"+month+"-"+day;
-	
-	//POPULATE FORM
-	$("#time2").val(currentTime);
-	$("#date2").val(currentDate);
+    var currentTime = hour + ":" + minutes;
+    var currentDate = date.getFullYear() + "-" + month + "-" + day;
+
+    //POPULATE FORM
+    $("#time2").val(currentTime);
+    $("#date2").val(currentDate);
 }
 
 
-function stopsValidation(){
-	console.log("stopsValidation");
-	$(".invalid").show();
+function stopsValidation() {
+    console.log("stopsValidation");
+    $(".invalid").show();
 }
 
-function routeValidation(){
-	if($("#starting").val() == ""){
-		console.log("Starting Fucked");
-		$(".start").show();
-	}
-	else if($("#destination").val() == ""){
-		$(".destination").show();
-	}
-	if($("#destination").val() == "" && $("#starting").val() == "")
-	{
-		$(".invalid").show();
-	}
+function routeValidation() {
+    if ($("#starting").val() == "") {
+        console.log("Starting Fucked");
+        $(".start").show();
+    }
+    else if ($("#destination").val() == "") {
+        $(".destination").show();
+    }
+    if ($("#destination").val() == "" && $("#starting").val() == "") {
+        $(".invalid").show();
+    }
 }
 
-function routeSubmit(){
-	var data = $("#allRoutes").val();
+function routeSubmit() {
+    var data = $("#allRoutes").val();
 }
 
---
+++ js/planTrip.js
@@ -0,0 +1,140 @@
+//planTrip
+//Kevin Ungerecht
+//audrey henry
+//refined and upgraded by TGB
+
+var SPOKANE_COORDINATES = {
+    lat: 47.6588,
+    lng: -117.4260
+};
+
+//this is the only function called from outside this file
+//TODO: encapsulate the rest
+function getTrips(origin, dest, date, time, sortArrivingElseDeparting) {
+
+    var directionsService = new google.maps.DirectionsService();
+    var timeFormatted = concatDateTime(date, time);
+    var options = {
+        modes: ['BUS'],
+        routingPreference: 'LESS_WALKING'
+    };
+    (sortArrivingElseDeparting)? options.arrivalTime = timeFormatted: options.departureTime = timeFormatted;
+
+    var request = {
+            origin: origin,
+            destination: dest,
+            travelMode: 'TRANSIT',
+            transitOptions: options,
+            provideRouteAlternatives: true
+        };
+
+    directionsService.route(request, onDirectionsReceived);
+}
+
+function onDirectionsReceived(results, status){
+    if (status !== 'OK') {
+        window.alert("Houston we have a problem");
+        console.error("directionsResult status: " + status);
+        return;
+    }
+
+    if (results.routes.length < 1) {
+        console.log("no direction results received");
+        return;
+    }
+
+    var selectedRoute = 0;
+    var firstResult = buildRouteFromIndex(results, 0);
+    var GMap = initializeGMap();
+    var directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(GMap);
+    directionsRenderer.setDirections(firstResult);
+    $("#map").show();
+
+    for (var r = 0; r < results.routes.length; r++) {
+        var routeResult = buildRouteFromIndex(results, r);
+        $("#routes").append(buildRouteHTML(routeResult.routes[0], r));
+
+        //add handlers to route rows
+        $('#rr' + r).click(function () {
+            var routeRowId = $(this).attr("id");
+            routeRowId = parseInt(routeRowId.slice(-1));
+
+            //don't redraw selected route
+            if (routeRowId === selectedRoute)
+                return;
+
+            directionsRenderer.setDirections(buildRouteFromIndex(results, routeRowId));
+            selectedRoute = routeRowId;
+        });
+
+        //add handlers to route "favorite" buttons
+        $('#pRoutesAddFave' + r).click(onRouteFavorited);
+    }
+
+    $("#routesList").show();
+}
+
+//builds a route object from a given index and set of results
+function buildRouteFromIndex(results, index){
+    return {
+        geocoded_waypoints: results.geocoded_waypoints,
+        request: results.request,
+        routes: [results.routes[index]],
+        status: results.status
+    };
+}
+
+function initializeGMap(){
+    return new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: SPOKANE_COORDINATES,
+        scrollwheel: false
+    });
+}
+
+function buildRouteHTML(routeResult, routeIndex) {
+    console.log(routeResult);
+    var stepString = "";
+    for (var step = 0; step < routeResult.legs[0].steps.length; step++) {
+        if (routeResult.legs[0].steps[step].travel_mode === "TRANSIT") {
+            stepString += '<i class="ui big bus icon pRouteBusIcon"></i>'; //<p> ' + routeResult.legs[0].steps[i].transit.line.short_name + ' </p> //TODO: integrate into display; make it look nice
+            if (step < routeResult.legs[0].steps.length - 1 && routeResult.legs[0].steps[step + 1].travel_mode === "TRANSIT") {
+                stepString += '<i class="arrow right icon"></i>';
+            }
+        }
+    }
+
+    var routeRowId = "rr" + routeIndex + "";
+    var fare = (routeResult.legs[0].hasOwnProperty("fare"))? '<p class="routeFare">Fare: ' + routeResult.fare.text + '</p>' : '';
+    var routeRow = '<div class="pRoutesRow" id="' + routeRowId + '">' +
+        stepString +
+        '<p class="routeDtoA">' + routeResult.legs[0].departure_time.text + ' - ' + routeResult.legs[0].arrival_time.text + '</p>' +
+        '<p class="routeDur">' + routeResult.legs[0].duration.text + '</p>' +
+        fare +
+        '<p class="routeDist">' + routeResult.legs[0].distance.text + '</p>' +
+        '</div><br>';
+    return routeRow;
+}
+
+//TODO: implement saving routes
+function onRouteFavorited(){
+    var routeRowRoute = $.parseJSON($(this).attr("value"));
+    console.log("route favorited: " + routeRowRoute);
+    //var routeRow = $(this).attr("id");
+    //routeRow = parseInt(routeRow.slice(-1));
+}
+
+function concatDateTime(date, time) {
+
+    var splitDate = date.split("-");
+    var year = splitDate[0];
+    var month = splitDate[1]--;
+    var day = splitDate[2];
+
+    var splitTime = time.split(":");
+    var hour = splitTime[0];
+    var min = splitTime[1];
+
+    return new Date(year, month, day, hour, min);
+}
